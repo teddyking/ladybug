13be45a11cb16c591931710121487dd5c6ffd2b0
teddyking <ed@teddyking.co.uk>
2016-10-28 17:26:46 +0100

    Add -h/--help flag to ladybug binary
    * Also add glide.{yaml,lock}
    * And add vendor dir
    
    [#133319085]

--- integration_test.go
+++ integration_test.go
@@ -6,15 +6,47 @@ import (
 
 	"os/exec"
 
+	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Integration", func() {
-	It("returns a 0 exit code", func() {
-		command := exec.Command(pathToLadybug)
-		session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
-		Expect(err).NotTo(HaveOccurred())
+	var (
+		args    []string
+		session *gexec.Session
+		stdout  *gbytes.Buffer
+	)
 
-		Eventually(session).Should(gexec.Exit(0))
+	BeforeEach(func() {
+		args = []string{}
+		stdout = gbytes.NewBuffer()
+	})
+
+	JustBeforeEach(func() {
+		var err error
+
+		command := exec.Command(pathToLadybug, args...)
+		session, err = gexec.Start(command, stdout, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	Context("when run without options or arguments", func() {
+		It("returns a 0 exit code", func() {
+			Eventually(session).Should(gexec.Exit(0))
+		})
+	})
+
+	Context("when run with -h/--help", func() {
+		BeforeEach(func() {
+			args = []string{"-h"}
+		})
+
+		It("prints usage info to stdout", func() {
+			Eventually(stdout).Should(gbytes.Say("Usage:"))
+		})
+
+		It("returns a 0 exit code", func() {
+			Eventually(session).Should(gexec.Exit(0))
+		})
 	})
 })
--- ladybug.go
+++ ladybug.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/jessevdk/go-flags"
+)
+
 func main() {
+	parser := flags.NewParser(nil, flags.Default)
+
+	_, err := parser.Parse()
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "%s\n", err)
+	}
 }
